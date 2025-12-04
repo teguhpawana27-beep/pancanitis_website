7f623d23a551baf6b6794cb3aff984aad8984651
teguhpawana27-beep <teguhpawana27@gmail.com>
2025-12-04 22:49:23 +0700

    script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Kode JavaScript untuk tombol CTA sudah dipindahkan ke index.html menggunakan 'onclick'.
+// Anda dapat menambahkan fungsionalitas lanjutan di sini jika diperlukan.
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Website PANCANITIS MOTOR Berhasil Dimuat!");
+});
